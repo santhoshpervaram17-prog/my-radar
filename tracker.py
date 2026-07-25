f560d3b1b9a6349e0a90becadcdc7cd1479c7fb0
santhoshpervaram17-prog <santhosh.pervaram17@gmail.com>
2026-07-25 22:12:20 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- tracker.py
+++ tracker.py
@@ -48,8 +48,8 @@ if "access_token" not in st.session_state:
 # ==========================================
 # 👇 PASTE YOUR DETAILS INSIDE THE QUOTES ON THESE 4 LINES 👇
 
-APP_ID = "Z9FB1LHW3W-100"                # <--- LINE 40: Paste Fyers App ID (e.g., "ABC123XYZ-100")
-SECRET_KEY = "1I254UJO93"            # <--- LINE 41: Paste Fyers Secret Key (Found next to App ID)
+APP_ID = "Y3YF5DNOVV-100"                # <--- LINE 40: Paste Fyers App ID (e.g., "ABC123XYZ-100")
+SECRET_KEY = "5GNRMNWCDG"            # <--- LINE 41: Paste Fyers Secret Key (Found next to App ID)
 REDIRECT_URI = "https://myradar.streamlit.app"          # <--- LINE 42: URL of your dashboard (e.g., "https://my-radar.streamlit.app/" or "http://localhost:8501/")
 DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1517513971744374949/_dHMw432cuIMEmEwEt-zSwj3Up8txjHiuKzwQ32TpBmeZmnol408YlxwT0sHkgAWdfjy"   # <--- LINE 43: Paste Discord Webhook URL here
 
